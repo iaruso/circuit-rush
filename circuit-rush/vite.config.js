ac9932383a6867ea942499b11ee3fb35416a8425
iaruso <iaruso.me@gmail.com>
2023-06-14 19:29:31 +0100

    Testing deploy

--- circuit-rush/vite.config.js
+++ circuit-rush/vite.config.js
@@ -3,20 +3,20 @@ import react from '@vitejs/plugin-react'
 const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
 
 export default {
-		base: './ciruit-rush/',
-    plugins:
-    [
-        react()
-    ],
-    root: 'src/',
-    publicDir: "../public/",
-    server: {
-        host: true,
-        open: !isCodeSandbox
-    },
-    build: {
-        outDir: '../dist',
-        emptyOutDir: true,
-        sourcemap: true
-    }
+	base: '/circuit-rush/',
+	plugins:
+	[
+		react()
+	],
+	root: 'src/',
+	publicDir: "../public/",
+	server: {
+		host: true,
+		open: !isCodeSandbox
+	},
+	build: {
+		outDir: '../dist',
+		emptyOutDir: true,
+		sourcemap: true
+	}
 }
