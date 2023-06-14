1a8dfbf874a28c6b5e60855edb06612f56809aba
iaruso <iaruso.me@gmail.com>
2023-06-14 19:23:10 +0100

    Vite config fix

--- circuit-rush/vite.config.js
+++ circuit-rush/vite.config.js
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react'
 const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
 
 export default {
+		base: './ciruit-rush/',
     plugins:
     [
         react()
     ],
     root: 'src/',
     publicDir: "../public/",
-    base: './',
     server: {
         host: true,
         open: !isCodeSandbox
