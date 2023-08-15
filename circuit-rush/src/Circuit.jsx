e3b3a6ed5bce1971cd18c191cb1abe5cef4899d2
iaruso <iaruso.me@gmail.com>
2023-08-15 23:26:34 +0100

    Minor improvements

--- circuit-rush/src/Circuit.jsx
+++ circuit-rush/src/Circuit.jsx
@@ -1,8 +1,8 @@
 import { Clone, useGLTF } from '@react-three/drei';
 import * as THREE from 'three';
 
-export default function Circuit() {
-  const { scene } = useGLTF('static/circuit.glb');
+export default function Circuit({perfomanceMode}) {
+  const { scene } = useGLTF(perfomanceMode ? 'static/circuit-min.glb' : 'static/circuit.glb');
   scene.traverse((child) => {
     if (child.isMesh) {
       child.material = new THREE.MeshStandardMaterial({
--- circuit-rush/src/MainMenu.jsx
+++ circuit-rush/src/MainMenu.jsx
@@ -76,7 +76,7 @@ const MainMenu = () => {
 
   return (
 		<div className="main-screen">
-			<img className="main-cover" ref={backgroundRef} src={'static/cover.webp'} alt='Circuit Rush Main Screen Cover' style={{ width: 'auto', height: 'auto' }}/>
+			<img className="main-cover" ref={backgroundRef} srcSet="static/cover-min.webp 768w, static/cover.webp 1024w" sizes="(max-width: 1024px) 100vw, 1024px" src="static/cover.webp" alt='Circuit Rush Main Screen Cover' style={{ width: 'auto', height: 'auto' }}/>
 			<div className="menu">
 				<div ref={textRef} className="start-info">
 					{!accelerationEnabled ? 'ENABLE GPU ACCELERATION IN YOUR BROWSER SETTINGS' : !mobileDevice ? 'PRESS ENTER TO START' : 'MOBILE DEVICES CURRENTLY NOT SUPPORTED'}
